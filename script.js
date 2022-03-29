c6b7eeb742f0b878cf5fecebf7a339c583db00a4
ravenjjj <102579937+ravenjjj@users.noreply.github.com>
2022-03-29 16:05:44 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let toggle = document.querySelector('.toggle')
-let navigation = document.querySelector('.navigation')
-
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active')
-  navigation.classList.toggle('active')
-})
